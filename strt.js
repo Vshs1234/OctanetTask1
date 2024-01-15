c9702f242baa32c0116b02bba596baa033746699
Velpuri Sai Harsha Sri <109352062+Vshs1234@users.noreply.github.com>
2024-01-15 11:29:43 +0530

    Add files via upload

---
+++ strt.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+// app.set("view engine","ejs");
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+app.listen(3000,function(){
+    console.log("succesffully running on server 3000");
+});
